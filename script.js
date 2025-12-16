a52d75a1b8c981aab28d3d192d937e29fcb6fd5c
Ravuvari Pavani <pavaniravuvari05@gmail.com>
2025-12-16 11:05:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = taskText + " <button onclick='deleteTask(this)'>X</button>";
+
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
